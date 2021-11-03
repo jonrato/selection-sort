a0590b1a022ad8207d54508adc7544e736ee2d6e
jonrato <jonatas_ferreira@usp.br>
2021-11-03 01:14:11 -0300

    busca ordenada de um vetor usando selection sort

---
+++ ordenacao-selection-sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertionSort(int v[200], int n){
+	int i, j, x;
+	for(i=1;i<n;i++){
+		x=v[i];
+		j=i-1;
+		while(j>=0 && v[j]>x){
+			v[j+1]=v[j];
+			j--;
+		}
+		v[j+1]=x;
+	}
+}
+
+int main(){
+	int v[200], n,i;
+	printf("Entre com o tamanho desejado do vetor:\n");
+	scanf("%d",&n);
+	printf("Entre com os %d elementos do vetor:\n",n);
+	for(i=0;i<n;i++){
+		scanf("%d", &v[i]);
+	}
+	insertionSort(v,n);
+	printf("\nVetor ordenado:\n");
+	for(i=0;i<n;i++){
+		printf("%d\t",v[i]);
+	}
+	printf("\n");
+	return 0;
+}
